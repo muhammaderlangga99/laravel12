a3f590308c482b254dc985382ee66e4623fac4e1
Muhammad Erlangga <muhammad.erlangga@trilogi.ac.id>
2025-02-27 18:10:42 +0700

    test: width login form

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -29,7 +29,7 @@ export default function AuthSplitLayout({ children, title, description }: AuthLa
                 )}
             </div>
             <div className="w-full lg:p-8">
-                <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div className="mx-auto flex w-[320px] flex-col justify-center space-y-6 sm:w-[350px]">
                     <Link href={route('home')} className="relative z-20 flex items-center justify-center lg:hidden">
                         <AppLogoIcon className="h-10 fill-current text-black sm:h-12" />
                     </Link>
