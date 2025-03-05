fc42a6ceae0d83ec25cf463504879a71d66a2e9c
Muhammad Erlangga <muhammad.erlangga@trilogi.ac.id>
2025-03-05 09:09:48 +0700

    modifify: mobile navbar

--- resources/js/components/MenuMobile.tsx
+++ resources/js/components/MenuMobile.tsx
@@ -31,7 +31,7 @@ export default function MenuMobile({ Navigation }: { Navigation: Navigation[] })
                     {Navigation.map((item: Navigation, index: number) => {
                         return (
                             <span key={index} className="flex flex-col items-start px-2">
-                                <Link href={item.link} className={`hover:text-zinc-600 w-full mb-2 dark:bg-zinc-900 px-2 py-1 rounded ${item.link == pathName && 'text-zinc-950 dark:text-red-600 dark:hover:text-zinc-900 font-extrabold hover:text-zinc-950'}`}>{item.name}</Link>
+                                <Link href={item.link} className={`hover:text-zinc-600 w-full mb-2 px-2 py-1 rounded ${item.link == pathName && 'text-zinc-950 dark:text-red-600 dark:hover:text-zinc-900 font-extrabold hover:text-zinc-950'}`}>{item.name}</Link>
                             </span>
                         );
                     })}
